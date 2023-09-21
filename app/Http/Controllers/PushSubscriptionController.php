97f16c8fa7aafed9aca9159340e685f31ae8a922
Jovert Palonpon <jovertical@gmail.com>
2023-09-21 22:38:27 +0800

    wip

---
+++ app/Http/Controllers/PushSubscriptionController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class PushSubscriptionController extends Controller
+{
+    public function store()
+    {
+        //
+    }
+
+    public function destroy()
+    {
+        //
+    }
+}
